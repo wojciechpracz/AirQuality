85c91829de50d9ca284bcee1990b00446b668272
wojciechpracz <pracz.wojciech@gmail.com>
2020-04-30 09:33:37 +0200

    Initial commit

---
+++ AirQuality/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AirQuality.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
